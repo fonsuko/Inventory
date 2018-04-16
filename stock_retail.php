e71e2c50dbb02e0b485b3afa1315cfe55fa2cfb8
Chanida K <fonsukoth@gmail.com>
2018-04-17 01:07:37 +0700

    QR code generated

--- stock_retail.php
+++ stock_retail.php
@@ -173,7 +173,7 @@
           </div>
           <div class="box-footer no-padding">
             <ul class="nav nav-stacked">
-              <li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
+              <li><a href="#">Price <span class="pull-right badge bg-blue"></span></a></li>
               <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
               <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
               <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
